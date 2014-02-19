4711b0c366b1b6abe1f6ddb56b1db9016bc03a3b
Rohan Kapadia <rohankapadia@gmail.com>
2014-02-19 11:20:12 +0530

    updated default java_home attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 default['jzmq']['url'] = "https://github.com/nathanmarz/jzmq/tarball/master"
-default['jzmq']['java_home'] = ""
+default['jzmq']['java_home'] = node[:java][:java_home]
--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ description      "Build/install jzmq"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "jdk"
+depends "java"
