11ecc2a0a268f8c1a7ddc425f53fd0f3220c7a7e
Rohan Kapadia <rohankapadia@gmail.com>
2014-02-19 19:34:43 +0530

    Removed jzmq_home. That has now changed in the recipe too.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,4 @@
-include_attribute "java"
-
 default['jzmq']['url'] = "https://github.com/nathanmarz/jzmq/tarball/master"
-default['jzmq']['java_home'] = node['java']['java_home']
 
 default['java']['jdk_version'] = '7'
 default['java']['install_flavor'] = 'oracle'
