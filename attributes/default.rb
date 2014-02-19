bd046f9fe36c89b99a07d85bd9ebc4a82bd3bcf8
Rohan Kapadia <rohankapadia@gmail.com>
2014-02-19 22:37:02 +0530

    removed java installation in default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,1 @@
 default['jzmq']['url'] = "https://github.com/nathanmarz/jzmq/tarball/master"
-
-default['java']['jdk_version'] = '7'
-default['java']['install_flavor'] = 'oracle'
-default['java']['oracle']['accept_oracle_download_terms'] = true
