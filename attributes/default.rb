a1cb31a7ddf890bcbe8fb4b5becf17e3e031d6f3
Rohan Kapadia <rohankapadia@gmail.com>
2014-02-19 15:45:15 +0530

    changed override to default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,7 @@ include_attribute "java"
 
 default['jzmq']['url'] = "https://github.com/nathanmarz/jzmq/tarball/master"
 default['jzmq']['java_home'] = node['java']['java_home']
-override['java']['jdk_version'] = '7'
-override['java']['install_flavor'] = 'oracle'
-override['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['java']['jdk_version'] = '7'
+default['java']['install_flavor'] = 'oracle'
+default['java']['oracle']['accept_oracle_download_terms'] = true
